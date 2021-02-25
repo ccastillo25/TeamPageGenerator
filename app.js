6c56558d5df6cb92149a9bd4ec1a13f6e5b8de3f
Carlos Castillo <castillo25ca@gmail.com>
2021-02-25 12:12:04 -0800

    updates

--- app.js
+++ app.js
@@ -106,9 +106,119 @@ function addIntern(){
     })
 }
 addIntern();
+function buildTeam(){
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+
+const render = require('./lib/htmlRender');
+const teamArray = [];
+
+function crossroads(){
+
+}
+function addManager(){
+    inquirer.prompt([
+        {
+           type:'input',
+           name:'managerName',
+           message:'What is your Manager\'s Name?'
+        },
+        {
+           type:'input',
+           name:'managerId',
+           message:'What is your Manager\'s Id?'
+        },        
+        {
+           type:'input',
+           name:'managerEmail',
+           message:'What is your Manager\'s email?'
+        },        
+        {
+           type:'input',
+           name:'officeNo',
+           message:'What is your Manager\'s officeno?'
+        },        
+    ]).then(res => {
+        console.log(res);
+        const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.officeNo);
+        teamArray.push(manager);
+        crossroads();
+    })
+}
+addManager();
+
+function addEngineer(){
+    inquirer.prompt([
+        {
+            type:'input',
+            name:'engineerName',
+            message:'What is your Engineer\'s Name?'
+        },
+        {
+            type:'input',
+            name:'engineerId',
+            message:'What is your Engineer\'s Id?'
+        },
+        {
+            type:'input',
+            name:'engineerEmail',
+            message:'What is your Engineer\'s Email?'
+        },
+        {
+            type:'input',
+            name:'github',
+            message:'What is your Engineer\'s Github?'
+        },
+    ]).then(res => {
+        console.log(res);
+        const engineer = new Engineer(res.engineerName, res.engineerId, res.engineerEmail, res.github);
+        teamArray.push(engineer);
+        crossroads();
+    })
+}
+addEngineer();
+
+function addIntern(){
+    inquirer.prompt([
+        {
+            type:'input',
+            name:'internName',
+            message:'What is your Intern\'s Name?'
+        },
+        {
+            type:'input',
+            name:'internId',
+            message:'What is your Intern\'s Id?'
+        },
+        {
+            type:'input',
+            name:'internEmail',
+            message:'What is your Intern\'s Email?'
+        },
+        {
+            type:'input',
+            name:'internSchool',
+            message:'What is your Intern\'s School?'
+        },
+    ]).then(res => {
+        console.log(res);
+        const intern = new Intern(res.internName, res.internId, res.internEmail, res.internSchool);
+        teamArray.push(intern);
+        crossroads();
+    })
+}
+addIntern();
 
 function buildTeam()
-    
+
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
